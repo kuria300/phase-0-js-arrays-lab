af827df00a7a266d716ff2a472a68a9510160781
Kuria300 <eugenekuria66@gmail.com>
2025-11-22 22:54:41 +0300

    changes

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,32 @@
 // Write your code here
+const products=["Laptop","Phone","Headphones","Monitor"];
 
+const logFirstProduct=()=>{
+   console.log(products[0])
+}
 
+//console.log(logFirstProduct(products))
+
+const addProduct=(product)=>{
+    console.log(products.push(product))
+}
+
+//console.log(addProduct('mouse'))
+//console.log(products)
+
+function updateProductName( index, newProduct){
+   return products[index]=newProduct
+}
+
+//console.log(updateProductName(products))
+//console.log(products)
+
+const removeLastProduct = function() {
+    return products.pop();
+}
+
+//console.log(removeLastProduct(products))
+//console.log(products)
 
 // Export the necessary parts for testing
 module.exports = {
